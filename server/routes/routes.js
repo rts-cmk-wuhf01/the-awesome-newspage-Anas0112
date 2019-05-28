ab2565ef5a45e72f17a04d4c28ed9c6009b5c493
Anas0112 <43467064+Anas0112@users.noreply.github.com>
2019-05-28 14:59:59 +0200

    Fixed Links

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,5 +3,17 @@ module.exports = (app) => {
    app.get('/', (req, res, next) => {
       res.render('home');
    });
+   app.get('/categories-post', (req, res, next) => {
+      res.render('categories-post');
+   });
+   app.get('/single-post', (req, res, next) => {
+      res.render('single-post');
+   });
+   app.get('/about', (req, res, next) => {
+      res.render('about');
+   });
+   app.get('/contact', (req, res, next) => {
+      res.render('contact');
+   });
 
 };
