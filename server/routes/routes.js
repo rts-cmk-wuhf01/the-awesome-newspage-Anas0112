e38cf21265a13d3f929ba571e88be609506e5ab2
Anas0112 <43467064+Anas0112@users.noreply.github.com>
2019-06-04 13:14:48 +0200

    date-and-time - Added in Routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,11 @@
 module.exports = (app) => {
 
+   const date = require('date-and-time');
+
    app.get('/', (req, res, next) => {
+
+      let now = new Date('2019-04-14 07:00:14');
+
       res.render('home');
    });
    app.get('/categories-post', (req, res, next) => {
