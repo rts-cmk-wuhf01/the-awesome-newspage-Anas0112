6dd5c5f487428e928a31a9ec90a01cefd2eda80c
Anas0112 <43467064+Anas0112@users.noreply.github.com>
2019-06-06 10:11:23 +0200

    Database - Henter Products & Games

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,20 @@
+const mysql = require('../config/mysql');
+
+const date = require('date-and-time');
+
 module.exports = (app) => {
 
-   const date = require('date-and-time');
+   app.get('/database',  async (req,res,next)=>{
+      let db = await mysql.connect();
+      let [products] = await db.execute('SELECT * FROM products');
+      let [games] = await db.execute('SELECT * FROM games');
+      db.end();
+
+      res.render('products', {
+         'products': products,
+         'games': games,
+      });
+   });
 
    app.get('/', (req, res, next) => {
 
@@ -8,18 +22,26 @@ module.exports = (app) => {
 
       res.render('home');
    });
+
+
    app.get('/categories-post', (req, res, next) => {
 
       let now = new Date('2019-04-14 07:00:14');
 
       res.render('categories-post');
    });
+
+
    app.get('/single-post', (req, res, next) => {
       res.render('single-post');
    });
+
+
    app.get('/about', (req, res, next) => {
       res.render('about');
    });
+
+
    app.get('/contact', (req, res, next) => {
       res.render('contact');
    });
